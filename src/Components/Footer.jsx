e915461be0d37f973516dad75d57259d0a2925c9
T4rsha <tarshamounic04@gmail.com>
2024-09-30 19:06:16 +0530

    done

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+           <footer className='py-3 bg-dark'>
+            <div className='container'>
+                <p className='m-0 text-center text-white'>
+                    copyright &copy;Tarsha 2024
+                </p>
+            </div>
+           </footer>
+        </div>
+    );
+};
+
+export default Footer;
